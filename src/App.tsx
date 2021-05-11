923b0cf589af4bfb4990d71e9f0584e0f882d9b9
MARK_MERSENSKI <mjmersenski@quad.com>
2021-05-10 20:54:06 -0500

    Added deploy code and update app page.

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          MKE Timber Designs
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Under Construction
       </header>
     </div>
   );
